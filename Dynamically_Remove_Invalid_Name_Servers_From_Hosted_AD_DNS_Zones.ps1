65edd750a1a900fd09a126d20fef9ccf09262ac0
creamers <wesley@wesleyk.me>
2016-03-19 22:25:51 -0400

    Create Dynamically_Remove_Invalid_Name_Servers_From_Hosted_AD_DNS_Zones.ps1
    #This script will dynamically remove invalid name servers from AD Integrated DNS zones that are non-default zones
    #Author Wesley Kirkland
    #Last Updated 03-19-2016

---
+++ Dynamically_Remove_Invalid_Name_Servers_From_Hosted_AD_DNS_Zones.ps1
@@ -0,0 +1,24 @@
+#This script will dynamically remove invalid name servers from AD Integrated DNS zones that are non-default zones
+#Author Wesley Kirkland
+#Last Updated 03-19-2016
+
+#PDCe
+$DC = (Get-ADDomain).pdcemulator
+
+#Get DNS Server Zones, excluding TrustAnchors and MSDSC zones
+$DNSServerADIntegratedZones = Get-DnsServerZone -ComputerName $DC | Where-Object -Property IsDsIntegrated -EQ -Value $true | Where-Object -Property ZoneType -EQ -Value 'Primary' | Where-Object -Property ZoneName -NotMatch -Value "TrustAnchors|msdcs"
+
+foreach ($Zone in $DNSServerADIntegratedZones) {
+    $DNSZoneNameServers = Get-DnsServerResourceRecord -ZoneName $Zone.ZoneName -RRType Ns
+
+    foreach ($NSEntry in $DNSZoneNameServers) {
+        Write-Host $NSEntry.RecordData.NameServer
+        Try {
+            [System.Net.Dns]::GetHostEntry($NSEntry.RecordData.NameServer.Trim('.'))
+        } Catch [System.Management.Automation.RuntimeException] {
+           #Remove the record if we can not resolve DNS for it
+           Write-Warning ("I wish to remove " + $NSEntry.RecordData.NameServer + " from the zone " + $Zone.ZoneName)
+           Remove-DnsServerResourceRecord -ZoneName $zone.ZoneName -ComputerName $DC -RRType Ns -Name "@" -RecordData $NSEntry.RecordData.NameServer -Confirm:$false
+        }
+    }
+}
