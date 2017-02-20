66344da9ea38618718d03a788661ab42fa8e8270
Wesley Kirkland <wesley_kirkland@teamhealth.com>
2017-02-20 16:00:23 -0500

    Added base64 encoding file

---
+++ Base64_Encode_For_Formatting.ps1
@@ -0,0 +1,12 @@
+﻿Write-Output 'This script will read files as bytes/hex and keep all the random encoding and stuff'
+$SourceFile = Read-Host 'What is the file that you would like to encode?'
+
+$Content = Get-Content -Path $SourceFile -Encoding Byte
+
+$Base64 = [System.Convert]::ToBase64String($Content)
+
+Write-Output 'Outputting the Base64 encoded text to your clipboard'
+$Base64 | clip
+
+Write-Output 'Use the following to decode it on the target system'
+[System.Convert]::FromBase64String($Content)
