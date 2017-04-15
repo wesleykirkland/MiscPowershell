a417b2ea23cdbdaed94d713e5ce75c70b25300ea
Littlejohn <Cdlittlejohn1@gmail.com>
2017-04-15 11:46:42 -0400

    Changed formatting, syntax, and flow

--- Event-Log-QA.ps1
+++ Event-Log-QA.ps1
@@ -16,52 +16,51 @@
 
 [CmdletBinding()]
 param(
-	[Parameter(Position=0, ValueFromPipeline=$true)]
-	$ComputerName = '.',
+    [Parameter(Position = 0, ValueFromPipeline = $true)]
+    $ComputerName = '.',
 
-    [Parameter(Position=1)]
+    [Parameter(Position = 1)]
     [String]$log = 'All'
 )
 
 $temp = "C:\temp\qa"
 $ComputerName | Set-Variable computername -Scope Script
 
-if ($ComputerName -eq '.') {$ComputerName = Read-Host "What is the Server you want to check?"} else {}
+if ($ComputerName -eq '.') {$ComputerName = Read-Host "What is the Server you want to check?"}
+else {}
 
 #Check if the computer is online
-$online = Test-Connection $computername -Count 1 -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | out-null
-#If statement if the computer is online continue else warn and exit the script
-if (!($?)) {
-    Write-Warning "Computer is not online"
+if (!(Test-Connection $ComputerName -Count 1 -Quiet)) {
+    Write-Warning "Computer is not online, exiting application"
     exit
 }
 
 function get-eventlogerrors {
     [CmdletBinding()]
     param(
-	    [Parameter(Position=0, ValueFromPipeline=$true)]
-	    $ComputerName = '.',
+        [Parameter(Position = 0, ValueFromPipeline = $true)]
+        $ComputerName = '.',
 
-        [Parameter(Position=1)]
+        [Parameter(Position = 1)]
         [String]$logName = '.'
     )
 
     #Get-EventLog Seems to be Faster, just uncomment one and comment the other if need be
     #Get-WinEvent -LogName $logName -ComputerName $ComputerName | where {($_.LevelDisplayName -eq "Error") -or ($_.LevelDisplayName -eq "Critical")} | select TimeCreated,Id,ProviderName,LevelDisplayName,Message | sort TimeCreated -Descending | export-csv $temp\$ComputerName-$logname.csv -NoTypeInformation
-    Get-EventLog -LogName $logName -ComputerName $ComputerName | where {($_.EntryType -eq "Error") -or ($_.EntryType -eq "Critical")} | select TimeGenerated,InstanceID,Source,EntryType,Message | sort TimeGenerated -Descending | export-csv $temp\$ComputerName-$logname.csv -NoTypeInformation
+    Get-EventLog -LogName $logName -ComputerName $ComputerName | Where-Object {($_.EntryType -eq "Error") -or ($_.EntryType -eq "Critical")} | Select-Object TimeGenerated, InstanceID, Source, EntryType, Message | Sort-Object TimeGenerated -Descending | export-csv $temp\$ComputerName-$logname.csv -NoTypeInformation -Force
 }
 
 function get-eventlogsize {
     [CmdletBinding()]
     param(
-	    [Parameter(Position=0, ValueFromPipeline=$true)]
-	    $ComputerName = '.',
+        [Parameter(Position = 0, ValueFromPipeline = $true)]
+        $ComputerName = '.',
 
-        [Parameter(Position=1)]
+        [Parameter(Position = 1)]
         [String]$logName = '.'
     )
     $eventlogsize = Get-WmiObject -ComputerName $ComputerName -Class Win32_NTEventLogFile -filter "LogFileName = '$logName'"
-    $eventlogsize.MaxFileSize | Set-Variable size -Scope Script
+    $eventlogsize.MaxFileSize | Set-Variable s5ize -Scope Script
 
     #Event Sizes to match log sizes
     $masterlogsize = '65536000'
@@ -69,9 +68,9 @@ function get-eventlogsize {
     #If statement to verify log size
     if ($masterlogsize -eq $size) {write-host "$logName Size is 64 Megs, you are good to go" | Set-Variable sizeconfirmed -Scope Script}
     else {
-    Write-Warning "Size is Not Right Please Check" | Set-Variable sizeconfirmed -Scope Script
-    $currentsize = $eventlogsize.MaxFileSize
-    Write-Warning "This current size is $currentsize"
+        Write-Warning "Size is Not Right Please Check" | Set-Variable sizeconfirmed -Scope Script
+        $currentsize = $eventlogsize.MaxFileSize
+        Write-Warning "This current size is $currentsize"
     }
 }
 
@@ -90,7 +89,8 @@ if ($log -eq 'All') {
     #User Message
     write-host "Event Log Output is stored in $temp\$ComputerName-*.csv"
     invoke-item $temp -ErrorAction SilentlyContinue
-} else {
+}
+else {
     #Run Functions Against the log that was specified.
     get-eventlogerrors $ComputerName $log
     get-eventlogsize $ComputerName $log
