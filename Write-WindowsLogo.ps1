6aa988c8ca934dae77235aa03f1ef0ba583dcb8b
Wesley Kirkland <wesley@wesleyk.me>
2017-04-17 09:40:48 -0400

    Added Write-WindowsLogo from PS Summit 2017

---
+++ Write-WindowsLogo.ps1
@@ -0,0 +1,62 @@
+﻿function Write-WindowsLogo {
+    Write-Host -Object ("")
+    Write-Host -Object ("")
+    Write-Host -Object ("         ,.=:^!^!t3Z3z.,                  ") -ForegroundColor Red
+    Write-Host -Object ("        :tt:::tt333EE3                    ") -ForegroundColor Red
+    Write-Host -Object ("        Et:::ztt33EEE ") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" @Ee.,      ..,     $Date") -ForegroundColor Green
+    Write-Host -Object ("       ;tt:::tt333EE7") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" ;EEEEEEttttt33#     ") -ForegroundColor Green
+    Write-Host -Object ("      :Et:::zt333EEQ.") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" SEEEEEttttt33QL     ") -NoNewline -ForegroundColor Green
+    Write-Host -Object ("      ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("      it::::tt333EEF") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" @EEEEEEttttt33F      ") -NoNewline -ForeGroundColor Green
+    Write-Host -Object ("          ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("     ;3=*^``````'*4EEV") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" :EEEEEEttttt33@.      ") -NoNewline -ForegroundColor Green
+    Write-Host -Object ("    ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("     ,.=::::it=., ") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object ("``") -NoNewline -ForegroundColor Red
+    Write-Host -Object (" @EEEEEEtttz33QF       ") -NoNewline -ForegroundColor Green
+    Write-Host -Object ("        ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("   ") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("    ;::::::::zt33) ") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object ("  '4EEEtttji3P*        ") -NoNewline -ForegroundColor Green
+    Write-Host -Object ("        ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("   :t::::::::tt33.") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (":Z3z.. ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object (" ````") -NoNewline -ForegroundColor Green
+    Write-Host -Object (" ,..g.        ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("       ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("           ") -ForegroundColor Cyan
+    Write-Host -Object ("   i::::::::zt33F") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (" AEEEtttt::::ztF         ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("     ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("  ;:::::::::t33V") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (" ;EEEttttt::::t3          ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("           ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("  E::::::::zt33L") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (" @EEEtttt::::z3F          ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("              ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object (" {3=*^``````'*4E3)") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (" ;EEEtttt:::::tZ``          ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("        ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("             ``") -NoNewline -ForegroundColor Cyan
+    Write-Host -Object (" :EEEEtttt::::z7            ") -NoNewline -ForegroundColor Yellow
+    Write-Host -Object ("               ") -NoNewline -ForegroundColor Red
+    Write-Host -Object ("") -ForegroundColor Cyan
+    Write-Host -Object ("                 'VEzjt:;;z>*``           ") -ForegroundColor Yellow
+    Write-Host -Object ("                      ````                  ") -ForegroundColor Yellow
+    Write-Host -Object ("")
+}
