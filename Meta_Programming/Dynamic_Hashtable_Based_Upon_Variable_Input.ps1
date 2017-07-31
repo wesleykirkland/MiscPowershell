9369cf15fddd913e59a3bae413da2e77bfbe70b0
Wesley Kirkland <wesley.kirkland@ministrybrands.com>
2017-07-31 10:43:39 -0400

    Meta Programming Hashtable

---
+++ Meta_Programming/Dynamic_Hashtable_Based_Upon_Variable_Input.ps1
@@ -0,0 +1,28 @@
+﻿#Setup some sample variables
+$varname1 = 1
+$varname2 = 2
+$varname3 = 3
+
+function MetaDemo1 ([string[]]$Varinput) {
+    #Find all variables
+    Write-Output "We have the following variables to test: $Varinput"
+    
+    #Build our Object
+    $Object = New-Object -TypeName psobject
+    foreach ($Var in $Varinput) {
+        $Object | Add-Member -MemberType NoteProperty -Name $Var -Value (Get-Variable -Name $Var).Value
+    }
+
+    #Output the Object
+    $Object
+}
+
+#Specify what the variables names are in a string without the $ variable indicator
+MetaDemo1 -Varinput 'varname1','varname2','varname3'
+
+#Sample Output
+<#
+varname1 : 1
+varname2 : 2
+varname3 : 3
+#>
